10f26178c2f5532c0eabd953a5e74875071802c6
Eugene-TechnoNinja <technoninja@ukr.net>
2024-01-11 16:47:30 +0200

    start of implementation of program logic

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from menu import *
+from colorama import Fore, Back, Style
+
+
+if __name__ == '__main__':
+    intro(Back.CYAN, Fore.MAGENTA, "Word Counter")
+    notifier(Fore.MAGENTA, "The program counts how often words appear in a text document.".upper())
 
---
+++ menu.py
@@ -0,0 +1,10 @@
+from colorama import Style
+
+
+def intro(bg_color, color, title):
+    MULTI = 25
+    print(bg_color + color + "#" * MULTI + f" {title} ".upper() + "#" * MULTI + Style.RESET_ALL)
+
+
+def notifier(color, text):
+    print(color + text + Style.RESET_ALL)
